5c9f190d9ae4a423444ddbd5586cac1517a84286
Guy <75747556+guydup@users.noreply.github.com>
2026-02-27 21:49:16 +0100

    pre-commit

--- scripts/assemble_DT16-1.py
+++ scripts/assemble_DT16-1.py
@@ -16,8 +16,12 @@ from failure_paths import ExcelEventGraph
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Assemble and integrate scenario fragility curves.")
-    parser.add_argument("--hr-path", type=Path, default=Path("C:/Users/SAKA/Downloads/DT16-1/hr/WBI2017_Benedenrijn_16-1_v04"))
-    parser.add_argument("--hr-calname", default="ws") # map to calculation name in HR folder, as subfolder of "Berekeningen" (e.g. "ws", "hs", etc.)
+    parser.add_argument(
+        "--hr-path", type=Path, default=Path("C:/Users/SAKA/Downloads/DT16-1/hr/WBI2017_Benedenrijn_16-1_v04")
+    )
+    parser.add_argument(
+        "--hr-calname", default="ws"
+    )  # map to calculation name in HR folder, as subfolder of "Berekeningen" (e.g. "ws", "hs", etc.)
     parser.add_argument("--dir-traject", type=Path, default=Path("C:/Users/SAKA/Downloads/DT16-1/"))
     parser.add_argument("--output-folder", type=Path, default=Path("C:/Users/SAKA/Downloads/DT16-1/output"))
     parser.add_argument("--scenario-name", default="scenario1")
