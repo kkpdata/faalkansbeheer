ebeec43b4034bab5818369312f5ee61ad8fcf694
Sander Kapinga <s.kapinga@wsrl.nl>
2026-06-26 18:48:31 +0200

    added some extra fields in result table

--- scripts/assemble_DT16-2.py
+++ scripts/assemble_DT16-2.py
@@ -350,6 +350,10 @@ def main() -> None:
     # add combined Pf to the dataframe (same value for each row)
     df_result1["Traject_Pf_bovengrens"] = bovengrens_pf
     df_result1["Traject_Pf_ondergrens"] = ondergrens_pf
+    # calculate percentage of each Vak_Section_Pf relative to the Traject_Pf_bovengrens
+    df_result1["Vak_Section_Pf_percentage"] = df_result1["Vak_Section_Pf"] / bovengrens_pf
+    #calculate N_traject
+    df_result1["N_traject"] = bovengrens_pf / ondergrens_pf
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H%M")
 
     df_result1.to_excel(output_folder / dir_traject.name / f"{dir_traject.name}_result_{timestamp}.xlsx", index=False)
