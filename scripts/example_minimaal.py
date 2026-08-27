577e193b4e9b80c1393301de40aa4fdb9ce1d872
Sander Kapinga <s.kapinga@wsrl.nl>
2026-08-27 11:49:54 +0200

    update

--- scripts/example_minimaal.py
+++ scripts/example_minimaal.py
@@ -1,9 +1,9 @@
 from failure_paths import ExcelEventGraph
 
-eeg = ExcelEventGraph.load("scripts/example_input/voorbeeld_minimaal.xlsx", "scenario1")
-eeg.plot(view=True, output_path="scripts/example_output/et_minimaal.png", water_level=3)
+eeg = ExcelEventGraph.load("scripts/example_input/dummy_traject/16-X_006_DP023-DP033.xlsx", "scenario1")
+eeg.plot(view=True, output_path="scripts/example_output/et_minimaal.png", water_level=5.6)
 # paths = eeg.get_failure_paths()
-_, fcs = eeg.get_failure_path_probabilities(water_levels=[3, 4])
+_, fcs = eeg.get_failure_path_probabilities(water_levels=[1,2,3, 4,5,6,7,8])
 
 for fc in fcs:
     print(fc.path)
